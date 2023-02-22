6c74508bbc6b88d2486b45bf1c475943605c6d42
romis44 <romankovalchuk44@gmail.com>
2023-02-22 16:40:39 +0200

    add style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StatisticsList, StatisticsItem } from './Statistics.styled';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <ul className="statList">
-      <li className="statItem">Good: {good}</li>
-      <li className="statItem">Neutral: {neutral}</li>
-      <li className="statItem">Bad: {bad}</li>
-      <li className="statItem">Total: {total}</li>
-      <li className="statItem">Positive feedback: {positivePercentage}%</li>
-    </ul>
+    <StatisticsList>
+      <StatisticsItem>Good: {good}</StatisticsItem>
+      <StatisticsItem>Neutral: {neutral}</StatisticsItem>
+      <StatisticsItem>Bad: {bad}</StatisticsItem>
+      <StatisticsItem>Total: {total}</StatisticsItem>
+      <StatisticsItem>Positive feedback: {positivePercentage}%</StatisticsItem>
+    </StatisticsList>
   );
 };
 
